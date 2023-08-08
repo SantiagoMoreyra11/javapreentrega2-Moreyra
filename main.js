c85018130f988023dbb20154dd3db4ab1ee54e07
Santiago Moreyra <santimoreyra89@gmail.com>
2023-08-08 01:02:37 -0300

    terminado

--- main.js
+++ main.js
@@ -8,46 +8,37 @@
 // debemos usar un método de array que busque a ese elemento, que tome su valor de nombre y precio y que mande un
 // alerta al usuario, diciéndole que el producto ya está preparado para ir a su domicilio, que salio x dinero y agradecer por su combra.
 
-class producto {
-  constructor(id, nombre, precio, cantiadad, categoria) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.cantiadad = cantiadad;
-    this.categoria = categoria;
-  }
+function Producto(id, nombre, precio, cantidad, categoria) {
+  this.id = id;
+  this.nombre = nombre;
+  this.precio = precio;
+  this.cantidad = cantidad;
+  this.categoria = categoria;
 }
 
-const producto = [];
+const productos = [];
 
-const producto1 = new producto(1, "proteina", 10000, 20, "suplementos");
-const producto2 = new producto(2, "creatina", 8200, 20, "suplementos");
-const producto3 = new producto(3, "quemadores", 5000, 20, "suplementos");
-const producto4 = new producto(4, "straps", 5000, 10, "accesorios");
-const producto5 = new producto(5, "cinturon", 7900, 15, "accesorios");
+productos.push(new Producto(1, "proteina", 10000, 15, "suplementos"));
+productos.push(new Producto(2, "creatina", 7000, 15, "suplementos"));
+productos.push(new Producto(3, "quemadores", 5000, 15, "suplementos"));
+productos.push(new Producto(4, "straps", 7000, 8, "indumentaria"));
+productos.push(new Producto(5, "cinturon", 6600, 10, "indumentaria"));
 
-producto.push(producto1, producto2, producto3, producto4, producto5);
+productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
 
-producto.sort((a, b) => a.nombre.localecompare(b.nombre));
+const productoSeleccionado = prompt("¿Qué producto deseas comprar?");
 
-const productoSeleccionado = prompt(
-  "¿Qué producto estas buscando? Ingrese el Nombre del Producto:"
-);
-
-const productoEncontrado = producto.find(
-  (producto) => preducto.nombre === productoSeleccionado
+const productoEncontrado = productos.find(
+  (producto) => producto.nombre === productoSeleccionado
 );
 
 if (productoEncontrado) {
-  const precioTotal = productoEncontrado.precio.toFixed(2);
-  alert(
-    `¡El producto "${productoEncontrado.nombre}" está Listo para ser despachado!\n\n` +
-      `Precio: ${precioTotal}\n` +
-      `Cantidad disponible: ${productoEncontrado.cantiadad}\n\n` +
-      "¡Gracias por su Compra que la disfrute!"
-  );
+  const costoTotal = productoEncontrado.precio;
+
+  const mensaje = `El producto "${productoEncontrado.nombre}" está listo para ser despachado. 
+    El costo total es $${costoTotal}. ¡Gracias por Confiar en nosotros!`;
+
+  alert(mensaje);
 } else {
-  alert(
-    "producto no Encontrado, Por Favor verifique su nombre e reintente de nuevo."
-  );
+  alert("Lo sientimos, el producto que busca no esta displonible.");
 }
